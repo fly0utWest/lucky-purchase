ce505ac387b757e0b13711b43d7ce72603623f8c
nikita f0w <shealtov@gmail.com>
2025-02-26 21:50:54 +0300

    ,igrations is not in repo now

--- api/src/services/user.service.ts
+++ api/src/services/user.service.ts
@@ -1,5 +1,8 @@
 import { prisma } from "../db/config";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET as string;
 
 export const createUser = async (login: string, password: string) => {
   const encryptedPassword = await bcrypt.hash(password, 10);
@@ -9,4 +12,10 @@ export const createUser = async (login: string, password: string) => {
 
 export const getUserByLogin = async (login: string) => {
   return prisma.user.findUnique({ where: { login } });
+};
+
+export const generateToken = async (userId: string) => {
+  return jwt.sign({ userId }, JWT_SECRET, {
+    expiresIn: "30 days",
+  });
 };
