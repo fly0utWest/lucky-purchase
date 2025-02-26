ec61ee672ca5fe1395424cfa4054b930d01c0e5b
nikita f0w <shealtov@gmail.com>
2025-02-26 14:09:26 +0300

    raw user registration is ready

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import "@dotenvx/dotenvx/config";
+import userRoutes from "./routes/user.routes";
 
 const app = express();
 app.use(express.json());
+
+app.use("/users", userRoutes);
 
 const PORT = process.env.PORT || 3000;
 
