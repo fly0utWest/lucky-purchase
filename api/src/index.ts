6b9d73fa315690381a7979c5ae170eefd1d0e3a2
nikita f0w <shealtov@gmail.com>
2025-02-26 22:52:18 +0300

    corses added

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import "@dotenvx/dotenvx/config";
 import userRoutes from "./routes/user.routes";
+import cors from "cors";
 
 const app = express();
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
 
 app.use("/users", userRoutes);
