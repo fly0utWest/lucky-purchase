20c611ebc23c411ad760181d3ee869c4e5ca04dd
nikita ablamskiy <shealtov@gmail.com>
2025-03-18 18:21:35 +0300

    web: not found page added

---
+++ web/src/app/[...catchall]/page.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { AlertTriangle } from "lucide-react";
+
+export default function NotFoundPage() {
+  const router = useRouter();
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen text-center p-4">
+      <AlertTriangle className="w-16 h-16 text-destructive" />
+      <h1 className="text-4xl font-bold mt-4">404 - Не найдено</h1>
+      <p className="mt-2">
+      Страница не найдена
+      </p>
+      <Button className="mt-6" onClick={() => router.push("/")}>
+        Домой
+      </Button>
+    </div>
+  );
+}
