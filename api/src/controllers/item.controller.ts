a2ad63f7bfdbe95f91d5d4fd92b9abf96cb51ac0
nikita ablamskiy <shealtov@gmail.com>
2025-03-19 20:03:11 +0300

    api: loggin item addition

--- api/src/controllers/item.controller.ts
+++ api/src/controllers/item.controller.ts
@@ -19,6 +19,7 @@ export async function registerItem(
       throw new AppError("Не удалось создать новое объявление", 400);
     }
 
+    console.log(`[SUCCESS]: Item was added by user with id ${req.userId}`)
     return res.status(201).json({ ...newItem });
   } catch (error) {
     next(error);
