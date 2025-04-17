d70b652ea08806a27613832f8b46559bc98fabc1
nikita ablamskiy <shealtov@gmail.com>
2025-04-17 19:40:00 +0300

    renamed validation middleware

---
+++ api/src/middlewares/validation.middleware.ts
@@ -0,0 +1,33 @@
+import { Request, Response, NextFunction } from "express";
+import { ZodError, ZodSchema } from "zod";
+
+export function validate(
+  schema: ZodSchema,
+  type: "body" | "params" | "userId" | "query" = "body"
+) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    let data;
+
+    switch (type) {
+      case "body":
+        data = req.body;
+        break;
+      case "params":
+        data = req.params;
+        break;
+      case "query":
+        data = req.query;
+        break;
+      default:
+        return res.status(400).json({ error: "Недопустимый тип валидации" });
+    }
+
+    const result = schema.safeParse(data);
+    if (!result.success) {
+      return next(new ZodError(result.error.errors));
+    }
+
+    res.locals.validatedData = result.data;
+    next();
+  };
+}
